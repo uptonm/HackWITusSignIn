a8939def92b629541d5aaf731caaf754c75445f2
Mike Upton <uptonm@wit.edu>
2018-10-18 13:36:24 -0400

    Added 2 more tests

--- tests/setup.js
+++ tests/setup.js
@@ -1,25 +1,26 @@
-const mongoose = require('mongoose');
-require('../models/user.model')
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("../models/user.model");
+require("dotenv").config();
 beforeEach(async () => {
   async function clearDB() {
     await Promise.all(
-      Object.keys(mongoose.connection.collections).map(async (key) => {
+      Object.keys(mongoose.connection.collections).map(async key => {
         return await mongoose.connection.collections[key].deleteMany({});
       })
     );
   }
- 
+
   if (mongoose.connection.readyState === 0) {
     await mongoose.connect(
-      `mongodb://${process.env.USER}:${process.env.PASS}@ds151402.mlab.com:51402/upton-auth`,
+      `mongodb://${process.env.USER}:${
+        process.env.PASS
+      }@ds151402.mlab.com:51402/upton-auth`,
       { useNewUrlParser: true }
     );
   }
   await clearDB();
 });
- 
-afterEach(async () => {
-  await mongoose.connection.close();
-});
 
+afterAll(async () => {
+  await mongoose.disconnect();
+});
