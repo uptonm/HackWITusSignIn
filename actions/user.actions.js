0554fa7fbb77d321fa2c36c59c84c8316372ed02
Mike Upton <uptonm@wit.edu>
2018-10-18 17:01:35 -0400

    Added more user routes

--- actions/user.actions.js
+++ actions/user.actions.js
@@ -1,22 +1,73 @@
 const mongoose = require("mongoose");
+const colors = require("colors");
 const User = require("../models/user.model");
 
-exports.get = (req, res) => {
-  res.send("Getting All");
+const error = {
+  400: {
+    message: "Error, bad request"
+  },
+  404: {
+    message: "Error, user not found"
+  }
 };
 
-exports.getOne = (req, res) => {
-  res.send("Getting one");
+consoleOutput = (route, request, status) => {
+  if (status === 200) {
+    console.log(`${route} - ${colors.blue(request)} - ${colors.green(status)}`);
+  } else {
+    console.log(`${route} - ${colors.blue(request)} - ${colors.red(status)}`);
+  }
 };
 
-exports.post = (req, res) => {
-  res.send("Posting one");
+exports.get = async (req, res) => {
+  const exists = await User.find({});
+  if (exists && exists.length > 0) {
+    consoleOutput("/api/users", "GET", 200);
+    return res.status(200).send(exists);
+  }
+  consoleOutput("/api/users", "GET", 400);
+  return res.status(404).send(error[404]);
 };
 
-exports.put = (req, res) => {
-  res.send("Putting one");
+exports.getOne = async (req, res) => {
+  const exists = await User.findById(req.params.id);
+  if (exists) {
+    consoleOutput(`/api/users/${req.params.id}`, "GET", 200);
+    return res.status(200).send(exists);
+  }
+  consoleOutput(`/api/users/${req.params.id}`, "GET", 400);
+  return res.status(404).send(error[404]);
 };
 
-exports.delete = (req, res) => {
-  res.send("Deleteing One");
+exports.post = async (req, res) => {
+  const exists = await User.findOne(req.body);
+  if (!exists || exists.length === 0) {
+    const user = await new User(req.body).save();
+    consoleOutput("/api/users", "POST", 200);
+    return res.status(200).send(user);
+  }
+  consoleOutput("/api/users", "POST", 400);
+  return res.status(400).send(error[400]);
+};
+
+exports.put = async (req, res) => {
+  const exists = await User.findById(req.params.id);
+  if (exists) {
+    const update = await User.findByIdAndUpdate(exists._id, req.body);
+    consoleOutput(`/api/users/${req.params.id}`, "PUT", 200);
+    return res.status(200).send(update);
+  }
+  consoleOutput(`/api/users${req.params.id}`, "PUT", 404);
+  return res.status(404).send(error[404]);
+};
+
+exports.delete = async (req, res) => {
+  const exists = await User.findById(req.params.id);
+  if (exists) {
+    const response = await User.findByIdAndDelete(exists._id);
+    consoleOutput(`/api/users/${req.params.id}`, "DELETE", 200);
+    return res.status(200).send({ message: `User ${req.params.id} deleted` });
+  }
+  consoleOutput(`/api/users/${req.params.id}`, "DELETE", 404);
+  return res.status(404).send(error[404]);
 };
